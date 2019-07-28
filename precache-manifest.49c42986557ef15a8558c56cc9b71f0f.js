f1fe4b019be3b120a597076e683913333be5416d
Rob Ross <rob@Home-iMac.local>
2019-07-28 16:54:49 -0700

    Updates

---
+++ precache-manifest.49c42986557ef15a8558c56cc9b71f0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "69aa7415548b32069649",
+    "url": "/Plane-Guesser/static/css/main.89c9ee99.chunk.css"
+  },
+  {
+    "revision": "69aa7415548b32069649",
+    "url": "/Plane-Guesser/static/js/main.0cabda26.chunk.js"
+  },
+  {
+    "revision": "462e10970fc37c83c879",
+    "url": "/Plane-Guesser/static/js/runtime~main.b50f50d8.js"
+  },
+  {
+    "revision": "a6ba8df5158b9f194b71",
+    "url": "/Plane-Guesser/static/css/2.31c1e2cb.chunk.css"
+  },
+  {
+    "revision": "a6ba8df5158b9f194b71",
+    "url": "/Plane-Guesser/static/js/2.abe55e13.chunk.js"
+  },
+  {
+    "revision": "2070303ea4fcc26b8c937012009e1084",
+    "url": "/Plane-Guesser/index.html"
+  }
+];
